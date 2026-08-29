2f1ab3831ff325f1ab8e4da9653a8852660d2a5d
Aarsh Vanjari <aarshvanjari1@gmail.com>
2026-08-29 23:26:00 +0530

    Prepare FastAPI backend for deployment

--- backend/main.py
+++ backend/main.py
@@ -28,9 +28,10 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://fortyguard-hackathon-26-3nwz.vercel.app",
+],
 
     allow_credentials=True,
 
